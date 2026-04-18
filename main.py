ccd0a2a46fbf8b341ca1240e52982315db80298e
Maurice Gift <founder@giftedtech.co.ke>
2026-04-18 14:49:36 +0300

    update main.py for Render deployment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import math
 import json
+import os
 import random
 
 app = Flask(__name__)
@@ -43,4 +44,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
